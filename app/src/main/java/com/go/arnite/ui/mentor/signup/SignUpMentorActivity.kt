7158bacd61bb7a5e2e693743cc2a75a2627ad5e9
reinhard <reinhardjsilalahi@gmail.com>
2020-02-24 18:44:13 +0700

    setting styles

--- app/src/main/java/com/go/arnite/ui/mentor/signup/SignUpMentorActivity.kt
+++ app/src/main/java/com/go/arnite/ui/mentor/signup/SignUpMentorActivity.kt
@@ -1,7 +1,6 @@
 package com.go.arnite.ui.mentor.signup
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.go.arnite.R
